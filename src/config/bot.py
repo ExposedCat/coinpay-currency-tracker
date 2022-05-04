5c3d2d17980abd0ec0ff5c73a172e4bbc204c137
ExposedCat <artem13.prokop@gmail.com>
2022-05-04 22:15:57 +0200

    ⭐️ Add notifications state toggling

--- src/config/bot.py
+++ src/config/bot.py
@@ -1,23 +1,37 @@
 from os import getenv
-from typing import Tuple
 
 from aiogram import Bot, Dispatcher, executor, types
 
 from filters.button_click_filter import ButtonClickFilter
 
 from config.localization import setup_locale_engine
+
 from handlers.start import handler as _cmd_start
+from handlers.main_menu import handler as _cmd_main_menu
 from handlers.subscriptions_list import handler as _cmd_subs_list
+from handlers.toggle_notifications import (
+    handler_on as _cmd_notify,
+    handler_off as _cmd_not_notify,
+)
 
 
 def _setup_handlers(dispatcher: Dispatcher):
-    dispatcher.register_message_handler(commands=['start'], callback=_cmd_start)
+    dispatcher.register_message_handler(commands='start', callback=_cmd_start)
     dispatcher.register_message_handler(
         _cmd_subs_list, ButtonClickFilter('button_subscriptions')
     )
+    dispatcher.register_message_handler(
+        _cmd_main_menu, ButtonClickFilter('button_main_menu')
+    )
+    dispatcher.register_message_handler(
+        _cmd_notify, ButtonClickFilter('button_enable_notifications')
+    )
+    dispatcher.register_message_handler(
+        _cmd_not_notify, ButtonClickFilter('button_disable_notifications')
+    )
 
 
-def init_bot() -> Tuple[Bot, Dispatcher]:
+def init_bot() -> tuple[Bot, Dispatcher]:
     bot = Bot(
         token=getenv('BOT_TOKEN'),
         parse_mode=types.ParseMode.HTML,
---
+++ src/handlers/toggle_notifications.py
@@ -0,0 +1,20 @@
+from data.type_aliases import TranslateFunc
+from aiogram.types import Message
+
+from services.keyboards import build_main_menu
+
+
+async def handler_on(message: Message, translate: TranslateFunc):
+    # TODO: Set user notifications_enabled to True
+    await message.answer(
+        translate('notifications_enabled'),
+        reply_markup=build_main_menu(translate, True),
+    )
+
+
+async def handler_off(message: Message, translate: TranslateFunc):
+    # TODO: Set user notifications_enabled to False
+    await message.answer(
+        translate('notifications_disabled'),
+        reply_markup=build_main_menu(translate, False),
+    )
