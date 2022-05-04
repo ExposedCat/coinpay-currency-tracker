f35af4e2bd7d0886486ddf6d1256c1beb8f8d679
ExposedCat <artem13.prokop@gmail.com>
2022-05-04 22:17:40 +0200

    ⭐️ Add buttons to manage subscriptions

--- src/handlers/subscriptions_list.py
+++ src/handlers/subscriptions_list.py
@@ -1,20 +1,40 @@
 from data.type_aliases import TranslateFunc
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton as Button,
+)
 
 
 async def handler(message: Message, translate: TranslateFunc):
     # TODO: Get real user subsriptions
     subscriptions = (
-        {'buy': 'BTC', 'sell': 'USDT', 'interval': 5},
-        {'buy': 'ETH', 'sell': 'USDT', 'interval': 10},
-        {'buy': 'XRP', 'sell': 'UAH', 'interval': 5},
+        {'buy': 'BTC', 'sell': 'USDT', 'interval': 5, 'id': '0'},
+        {'buy': 'ETH', 'sell': 'USDT', 'interval': 10, 'id': '1'},
+        {'buy': 'XRP', 'sell': 'UAH', 'interval': 5, 'id': '2'},
     )
-    subscriptions_string = '\n'.join(
-        [translate('component_subscription', data) for data in subscriptions]
-    )
-    response_string = translate('subscriptions', {'list': subscriptions_string})
-    # TODO: Send each subscription as separate message
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    buttons = [
+        translate('button_add_notification'),
+        translate('button_main_menu'),
+    ]
+    keyboard.add(*buttons)
+    data = {
+        'is_empty': '' if len(subscriptions) else translate('component_empty')
+    }
     await message.answer(
-        response_string,
-        # TODO: Add button to add subscription
+        translate('subscriptions', data),
+        reply_markup=keyboard,
     )
+    for subscription in subscriptions:
+        keyboard = InlineKeyboardMarkup()
+        button = Button(
+            text=translate('button_remove_notification'),
+            callback_data=f'remove_notification_{id}',
+        )
+        keyboard.add(button)
+        await message.answer(
+            translate('component_subscription', subscription),
+            reply_markup=keyboard,
+        )
